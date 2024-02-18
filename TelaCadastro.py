efe545b8b1984fb9e72a10d4f9e6869073b584c5
Gabriel Isper <gabrielisperb@gmail.com>
2024-02-18 18:04:55 -0300

    Esqueci de commitar antes :)

---
+++ TelaCadastro.py
@@ -0,0 +1,69 @@
+import sqlite3
+
+# Declaração de Variáveis
+fimloop1 = 0
+fimloop2 = 0
+
+# Início do Programa
+print("Olá, bem vindo a tela de login da FEMA!")
+initchoice = int(input("Digite 1 para fazer login e 2 para se cadastrar: "))
+
+# Início da Lógica do Login
+if initchoice == 1:
+    while fimloop1 == 0:
+
+        # Coleta de Dados
+        ra = int(input("Digite seu RA: "))
+        senha = str(input("Digite sua senha: "))
+
+        # Verificação de Segurança
+        try:
+            conn = sqlite3.connect("FemaDB.db")
+            cursor = conn.cursor()
+
+            cursor.execute(f"SELECT ra, senha FROM alunos WHERE ra = {ra}")
+            result = cursor.fetchone()
+            
+            # Resultado da verificação
+            if result:
+                ra_db, senha_db = result
+                
+                if ra == ra_db and senha == senha_db:
+                    cursor.execute(f"SELECT nome FROM alunos WHERE ra = {ra}")
+                    nome = cursor.fetchone()
+                    nomeformatado = ''.join(map(str, nome[0]))
+
+                    print(f"Você está logado como: {nomeformatado}")
+                    
+                    logchoice = int(input("Digite 1 para conferir suas matérias e 2 para sair: "))
+                    
+                    if logchoice == 1:
+                        cursor.execute(f"SELECT curso FROM alunos WHERE ra = {ra}")
+                        curso_aluno = cursor.fetchall()
+                        cformatado = '\n'.join(map(str, curso_aluno[0]))
+
+                        cursor.execute(f"SELECT materia1, materia2, materia3, materia4, materia5, materia6, materia7 FROM cursos WHERE nome_curso = '{cformatado}'")
+                        materias = cursor.fetchone()
+
+                        mformatadas = ''.join(map(str, materias[0]))
+                        print("Suas matérias são: ", mformatadas)
+
+                        print(f"Seja bem vindo a FEMA {nomeformatado}! Finalizando conexão.")
+                        fimloop1 = 1
+
+                    elif logchoice == 2:
+                        print("Finalizando.")
+                        conn.close()
+                        fimloop1 = 1
+                    
+                    else:
+                        print("Comando inválido, finalizando.")
+                        conn.close()
+                        fimloop1 = 1
+            
+            else:
+                print("RA ou senha inválidos!")
+
+        # Tratamento de Erros
+        except sqlite3.Error as error:
+            print("Ocorreu um erro: ", error)
