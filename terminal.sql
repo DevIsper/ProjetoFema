a260bc2981e17a24c952ecba6b94fec7affdeb15
Gabriel Isper <gabrielisperb@gmail.com>
2024-02-18 16:38:29 -0300

    initial commit

---
+++ terminal.sql
@@ -0,0 +1,36 @@
+-- CREATE TABLE alunos (
+
+--     ra INT PRIMARY KEY NOT NULL,
+--     senha TEXT,
+--     nome TEXT,
+--     curso TEXT
+
+-- );
+
+-- CREATE TABLE cursos (
+
+--     nome_curso TEXT PRIMARY KEY,
+--     materia1 TEXT,
+--     materia2 TEXT,
+--     materia3 TEXT,
+--     materia4 TEXT,
+--     materia5 TEXT,
+--     materia6 TEXT,
+--     materia7 TEXT
+
+-- );
+
+-- CREATE TABLE professores (
+
+-- cpf INT PRIMARY KEY NOT NULL,
+-- senha TEXT,
+-- nome_professor,
+-- tipo_curso TEXT
+
+-- );
+
+-- INSERT INTO cursos (nome_curso, materia1, materia2, materia3, materia4, materia5, materia6, materia7)
+-- VALUES ('BCC', 'Algoritmos e Estruturas de Dados', 'Introdução a Lógica', 'Estatística', 'Matematica 1', 'Introdução a Computação', 'Matematica Discreta', 'Inglês Técnico');
+
+
+-- SELECT * FROM cursos;
